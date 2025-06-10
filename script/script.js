8feb2f2788b97226cee4077ca67dc93ae2fdc4ec
Shobhit Rawat <shobhit1948@gmail.com>
2025-06-10 21:00:55 +0530

    second commit

--- script/script.js
+++ script/script.js
@@ -6,13 +6,15 @@ searchBtn.addEventListener("click", fetchJob);
 async function fetchJob() {
   const input = search.value.trim().toLowerCase(); // normalize input
   try {
-    const response = await fetch(" https://remotive.io/api/remote-jobs");
+    const response = await fetch(" https://remotive.com/");
 
     if (!response.ok) {
       throw new Error("Job Not Found");
     }
 
     const data = await response.json();
+    console.log(data);
+    
     const jobs = data.data;
 
   } catch (error) {
